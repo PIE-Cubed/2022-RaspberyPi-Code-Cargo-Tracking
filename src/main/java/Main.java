d67158e03e277bcb0d99788eb85863772dcedb43
Alex Pereira <59519524+Mario13546@users.noreply.github.com>
2022-01-29 22:24:28 -0500

    Committing to continue work on another laptop
    Signed-off-by: Alex Pereira <59519524+Mario13546@users.noreply.github.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@
  * Raspberry Pi Code for FRC Team 2199.
  * @author Alex and Allwyn Pereira
  */
-//import java.io.FileWriter;
-//import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -23,7 +23,7 @@ import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoSource;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.vision.VisionThread;
-import edu.wpi.first.networktables.*;
+//import edu.wpi.first.networktables.*;
 
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
@@ -85,7 +85,7 @@ public final class Main {
   private static double centerX;
   private static final Object imgLock = new Object();
 
-  private static NetworkTable table;
+  //private static NetworkTable table;
 
   private static double emptyCount = 0;
 
@@ -228,9 +228,9 @@ public final class Main {
     }
 
     //Create a buffered file writer (rPi must be in writable mode for this to work)
-    //BufferedWriter writer = new BufferedWriter(new FileWriter("NetworkTable.txt"));
+    BufferedWriter writer = new BufferedWriter(new FileWriter("NetworkTable.txt"));
     
-    // Start the NetworkTables instance
+    /*// Start the NetworkTables instance
     NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
     if (server) {
       System.out.println("Setting up NetworkTables server");
@@ -239,7 +239,7 @@ public final class Main {
       System.out.println("Setting up NetworkTables client for team " + team);
       ntinst.startClientTeam(team);
     }
-    table = ntinst.getTable("TrackingValues");
+    table = ntinst.getTable("TrackingValues");*/
     
     // Start Cameras
     List<VideoSource> cameras = new ArrayList<>();
@@ -248,17 +248,26 @@ public final class Main {
     }
 
     // Gets the target color from a sendable chooser on the dashboard
-    NetworkTableEntry targetColor = table.getEntry("TargetColor");
-    String color0 = targetColor.toString();
-    //String color = "red";
+    //NetworkTableEntry targetColor = table.getEntry("TargetColor");
+    //String color = targetColor.toString();
+    String color = "red";
     
-    CargoTracking.setTargetColor(color0);
+    CargoTracking.setTargetColor(color);
 
     // Start image processing on camera 0 if present
     if (cameras.size() >= 1) {
       VisionThread visionThread = new VisionThread(cameras.get(0), new RedCargoTracking(), pipeline -> {
-        NetworkTableEntry isEmpty = table.getEntry("IsEmpty");
-        isEmpty.setBoolean(pipeline.filterContoursOutput().isEmpty());
+        //NetworkTableEntry isEmpty = table.getEntry("IsEmpty");
+        //isEmpty.setBoolean(pipeline.filterContoursOutput().isEmpty());
+        boolean isEmpty = pipeline.filterContoursOutput().isEmpty();
+
+        try {
+          writer.write("isEmpty: " + isEmpty + " ");
+          writer.flush();
+        }
+        catch (IOException ioe) {
+          ioe.printStackTrace();
+        }
 
         if (!pipeline.filterContoursOutput().isEmpty()) {
           //Reset emptyCount
@@ -272,18 +281,18 @@ public final class Main {
           synchronized (imgLock) {
             centerX = cameraFOV.x + (cameraFOV.width / 2);                   
             
-            NetworkTableEntry target = table.getEntry("CenterX");
-            target.setDouble( centerX );
+            //NetworkTableEntry target = table.getEntry("CenterX");
+            //target.setDouble( centerX );
 
             pipeline.filterContoursOutput().clear();
 
-           /*try {
+           try {
               writer.write("CenterX : " + centerX + ", Count : " + pipeline.findContoursOutput().size() + "\n");
               writer.flush();
             }
             catch (IOException ioe) {
               ioe.printStackTrace();
-            }*/
+            }
           }
         }
         else {
@@ -291,10 +300,9 @@ public final class Main {
           emptyCount++;
 
           //Sets the value of the NetwtorkTable Entry
-          NetworkTableEntry empty = table.getEntry("Empty");
-          empty.setDouble( emptyCount );
-        }
-        /*else {
+          //NetworkTableEntry empty = table.getEntry("Empty");
+          //empty.setDouble( emptyCount );
+
           try {
             writer.write("Empty Count: " + emptyCount + "\n");
             emptyCount++;
@@ -302,8 +310,8 @@ public final class Main {
           }
           catch (IOException ioe) {
             ioe.printStackTrace();
-          }  
-        }*/
+          }
+        }
       });
 
       visionThread.start();
